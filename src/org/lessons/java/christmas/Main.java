55ac89986f68a47dc6130f4aaa991c3af7b7819f
PinoMacri <pinonegozio1@outlook.it>
2023-05-12 12:05:53 +0200

    fixed

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -1,21 +1,29 @@
 package org.lessons.java.christmas;
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
-
-
 
 public class Main {
 	public static void main(String[] args) {
 		List<String> listaDesideri = new ArrayList<>();
 		Scanner desiderio = new Scanner(System.in);
 		String desiderios = "";
-		do  {
+		do {
 			System.out.println("Scrivi un desiderio");
 			desiderios = desiderio.nextLine();
 			listaDesideri.add(desiderios);
-			System.out.println("Attualmente hai " + listaDesideri.size() + " desideri");
+			int conteggio = listaDesideri.size();
+			if (desiderios.equals("basta")) {
+				listaDesideri.remove("basta");
+				conteggio = conteggio - 1;
+				System.out.println("In totale hai espresso " + conteggio + " desideri");
+			} else System.out.println("Attualmente hai " + conteggio + " desideri");
 		} while (!desiderios.equals("basta"));
-		System.out.println(listaDesideri);
+		for (String element : listaDesideri) {
+			System.out.println("-- " + element);
+		}
+
+		System.out.println("Lista stampata: " + listaDesideri);
 	}
 }
