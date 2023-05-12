fe2ff60ef29fe109cbc475b46da6898e02a02782
PinoMacri <pinonegozio1@outlook.it>
2023-05-12 15:03:42 +0200

    fixed

--- src/org/java/mymap/MainMap.java
+++ src/org/java/mymap/MainMap.java
@@ -7,7 +7,7 @@ public class MainMap {
 	public static void main(String[] args) {
 		Map<String, Integer> myMap = new HashMap<>();
         String str = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam - repudiandae est, alias in ullam excepturi ipsam modi odio Lorem ipsum dolor asperiores voluptatum, # quam distinctio ! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam repudiandae Totam repudiandae est ; accusantium voluptatum, quam distinctio aut magnam quod veniam esse nesciunt debitis.";
-        String[] words = str.split("\\W+");
+        String[] words = str.split(" ");
 
         for (String word : words) {
             if (myMap.containsKey(word)) {
@@ -20,8 +20,6 @@ public class MainMap {
         
 	
 	}
-	 public static String[] splitString(String input) {
-	        return input.split(" ");
-	    }
+	
 	
 }
