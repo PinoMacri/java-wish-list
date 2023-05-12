8e8769a6ecc20ba1adda52ea36e4c54bf1233d6b
PinoMacri <pinonegozio1@outlook.it>
2023-05-12 11:50:08 +0200

    esercizio consegnato

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -1,7 +1,21 @@
 package org.lessons.java.christmas;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 
 public class Main {
 	public static void main(String[] args) {
-
+		List<String> listaDesideri = new ArrayList<>();
+		Scanner desiderio = new Scanner(System.in);
+		String desiderios = "";
+		do  {
+			System.out.println("Scrivi un desiderio");
+			desiderios = desiderio.nextLine();
+			listaDesideri.add(desiderios);
+			System.out.println("Attualmente hai " + listaDesideri.size() + " desideri");
+		} while (!desiderios.equals("basta"));
+		System.out.println(listaDesideri);
 	}
 }
